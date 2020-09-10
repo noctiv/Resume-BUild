d8ac96d816b3543d3729a3031521dc03df23eb91
unknown <noctiv>
2020-09-10 21:37:33 +0530

    Updates

---
+++ precache-manifest.981a10bf107d16b2b092109264541395.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ac2b8261c07c33f41f",
+    "url": "/me/static/css/main.9d6ce35b.chunk.css"
+  },
+  {
+    "revision": "e4ac2b8261c07c33f41f",
+    "url": "/me/static/js/main.e4ac2b82.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e560a4232718fc721c1ac04e7d4778d1",
+    "url": "/me/index.html"
+  }
+];
